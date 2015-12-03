cdc86e0d92f52f91567883bfeca70bf0ac5a72e4
Hashma <shahi22h@mtholyoke.edu>
2015-12-03 14:41:23 -0500

    added classifier and tweepy

---
+++ classifier.py
@@ -0,0 +1,31 @@
+pos_tweets = [('I love this car', 'positive'),
+              ('This view is amazing', 'positive'),
+              ('I feel great this morning', 'positive'),
+              ('I am so excited about the concert', 'positive'),
+              ('He is my best friend', 'positive')]
+
+neg_tweets = [('I do not like this car', 'negative'),
+              ('This view is horrible', 'negative'),
+              ('I feel tired this morning', 'negative'),
+              ('I am not looking forward to the concert', 'negative'),
+              ('He is my enemy', 'negative')]
+
+tweets = []
+for (words, sentiment) in pos_tweets + neg_tweets:
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
+    tweets.append((words_filtered, sentiment))
+
+tweets = [
+    (['love', 'this', 'car'], 'positive'),
+    (['this', 'view', 'amazing'], 'positive'),
+    (['feel', 'great', 'this', 'morning'], 'positive'),
+    (['excited', 'about', 'the', 'concert'], 'positive'),
+    (['best', 'friend'], 'positive'),
+    (['not', 'like', 'this', 'car'], 'negative'),
+    (['this', 'view', 'horrible'], 'negative'),
+    (['feel', 'tired', 'this', 'morning'], 'negative'),
+    (['not', 'looking', 'forward', 'the', 'concert'], 'negative'),
+    (['enemy'], 'negative')]
+
+
+print tweets
