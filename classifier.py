810d53de45d03ec06e388854c983936dadd55739
Nikita Khan <nikitakhan@n244-123.mtholyoke.edu>
2015-12-09 18:51:41 -0500

    new classifier added

--- classifier.py
+++ classifier.py
@@ -1,3 +1,4 @@
+#hardcoded tweets to start off with
 pos_tweets = [('I love this car', 'positive'),
               ('This view is amazing', 'positive'),
               ('I feel great this morning', 'positive'),
@@ -10,22 +11,22 @@ neg_tweets = [('I do not like this car', 'negative'),
               ('I am not looking forward to the concert', 'negative'),
               ('He is my enemy', 'negative')]
 
+test_tweets = [('I feel happy this morning', 'positive'),
+               ('Larry is my friend', 'positive'),
+               ('I do not like that man', 'negative'),
+               ('My house is not great', 'negative'),
+               ('Your song is annoying', 'negative')]
+
+#create a list of tuples - each containing 2 elements - 1. list containing the words and 2. type of sentiment
+#get rid of words less than 2 characters
 tweets = []
 for (words, sentiment) in pos_tweets + neg_tweets:
     words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
     tweets.append((words_filtered, sentiment))
 
-tweets = [
-    (['love', 'this', 'car'], 'positive'),
-    (['this', 'view', 'amazing'], 'positive'),
-    (['feel', 'great', 'this', 'morning'], 'positive'),
-    (['excited', 'about', 'the', 'concert'], 'positive'),
-    (['best', 'friend'], 'positive'),
-    (['not', 'like', 'this', 'car'], 'negative'),
-    (['this', 'view', 'horrible'], 'negative'),
-    (['feel', 'tired', 'this', 'morning'], 'negative'),
-    (['not', 'looking', 'forward', 'the', 'concert'], 'negative'),
-    (['enemy'], 'negative')]
+test_tweets2 = []
+for (words, sentiment) in test_tweets:
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+    test_tweets2.append((words_filtered, sentiment))
 
-
-print tweets
+print test_tweets2
